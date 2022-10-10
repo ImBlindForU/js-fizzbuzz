675caa315dc7886d1c73c911f2ec7f7865931d10
ImBlindForU <samuele.zanchi10@gmail.com>
2022-10-10 16:29:32 +0200

    buzzfizz

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Scrivi un programma che stampi in console i numeri da 1 a 100,
+// ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+// programma che stampa i numeri da 1 a cento
+
+// SE il numero e multiplo di 3
+//     SE e anche multiplo di 5 allora metti fizz buzz
+//     ALTRIMENMTI metti buzz
+// ALTRIMENMTI SE e multiplo di 5
+//      metti fizz
+//      ALTRIMETNI non mettere niente 
+
+for (let i = 0; i <=100; i++){
+    let buzzFizz = "";
+    if (i % 3 == 0){
+        if (i % 5 == 0){
+            buzzFizz = "buzzFizz";
+        } else {
+            buzzFizz = "fizz";
+        }
+    } else if(i % 5 == 0){
+        buzzFizz = "buzz";
+    } else{
+        buzzFizz = "";
+    }
+    console.log(i, buzzFizz);
+}
+
+
